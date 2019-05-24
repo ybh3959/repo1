e170b23c0740055ed2522badfc85919672f8ff67
ybh3959 <48540947+ybh3959@users.noreply.github.com>
2019-05-24 18:24:42 +0800

    Update Test.java
    System.out.println(qweqweqwe);

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println(111222221);
         System.out.println(qweqweqwe);
+System.out.println(qweqweqwe);
     }
 
 }
