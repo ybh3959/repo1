4035f9b1d47b82f083a8ac2a7b3a7f6c7b8f3e0d
ybh <a2674297528@outlook.com>
2019-05-24 19:09:07 +0800

    System.out.println(12312312);

--- Test.java
+++ Test.java
@@ -2,8 +2,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println(111222221);
         System.out.println(qweqweqwe);
-        System.out.println(dqwdqw);
-        System.out.println(12312dqwdqwdwq312);
+
     }
 
 }
